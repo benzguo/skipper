7f40f49fd27029d4ed6e1a18f5887d932baeecd5
Ben Guo <ben@venmo.com>
2015-04-05 21:59:39 -0400

    .

--- skipper.rb
+++ skipper.rb
@@ -2,19 +2,36 @@ require 'timers'
 
 timers = Timers::Group.new
 counter = 0
-target_min = 30
+target_min = 31 
 buffer_range = 10
 target = target_min
 
-every_five_seconds = timers.every(1) { 
+def check_repeat()
+	current_repeat = `osascript -e 'tell application "Spotify" to repeating'`
+	if current_repeat.include? "false"
+		`osascript -e 'tell application "Spotify" to set repeating to not repeating'`
+		puts "turned on repeat"
+	end
+end
+
+def check_shuffle()
+	current_shuffle = `osascript -e 'tell application "Spotify" to shuffling'`
+	if current_shuffle.include? "false"
+		`osascript -e 'tell application "Spotify" to set shuffling to not shuffling'`
+		puts "turned on shuffle"
+	end
+end
+
+check_repeat()
+check_shuffle()
+
+timers.every(1) { 
 	counter += 1
 	if counter == target
-		puts counter
-		if counter % 2 == 0 
-			system "spotify next"
-		else 
-			system "spotify prev"
-		end
+		check_repeat()
+		check_shuffle()
+		puts "waited #{counter}s"
+		`osascript -e 'tell application "Spotify" to next track'`
 		target = target_min + rand(buffer_range)
 		counter = 0
 	end
